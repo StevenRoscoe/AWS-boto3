78688f0d4ea87d50427a1f5597b02bccd631d52f
Steven Roscoe <dbzroscoe@gmail.com>
2022-11-16 19:28:54 +0000

    Done

---
+++ Lambda/Backing_Up_EC2_Instances_Prune.py
@@ -0,0 +1,60 @@
+#PRUNE BACKUPS
+#Role needed:
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:DeleteSnapshot",
+        "ec2:Describe*",
+        "ec2:ModifySnapshotAttribute",
+        "ec2:ResetSnapshotAttribute"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+
+
+#Code needed:
+import boto3
+
+
+def lambda_handler(event, context):
+
+    account_id = boto3.client('sts').get_caller_identity().get('Account')
+    ec2 = boto3.client('ec2')
+    regions = [region['RegionName']
+               for region in ec2.describe_regions()['Regions']]
+
+    for region in regions:
+        print("Region:", region)
+        ec2 = boto3.client('ec2', region_name=region)
+        response = ec2.describe_snapshots(OwnerIds=[account_id])
+        snapshots = response["Snapshots"]
+
+        # Sort snapshots by date ascending
+        snapshots.sort(key=lambda x: x["StartTime"])
+
+        # Remove snapshots we want to keep (i.e. 3 most recent)
+        snapshots = snapshots[:-3]
+
+        for snapshot in snapshots:
+            id = snapshot['SnapshotId']
+            try:
+                print("Deleting snapshot:", id)
+                ec2.delete_snapshot(SnapshotId=id)
+            except Exception as e:
+                print("Snapshot {} in use, skipping.".format(id))
+                continue
