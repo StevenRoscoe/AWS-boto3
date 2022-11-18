93c9dfe6b09c75d01c049121ffd7c1e7684f940d
Steven Roscoe <dbzroscoe@gmail.com>
2022-11-18 20:56:19 +0000

    Done

--- Week_15_Project.py
+++ Week_15_Project.py
@@ -13,3 +13,23 @@ response = sqs.create_queue(
 )
 
 print(response['QueueUrl'])
+
+
+#Lambda Code
+import json                                 #transfers data into the JSON format
+import boto3                                #imports AWS' Python SDK into the code so it will run
+from datetime import datetime               #allows us to get the time and send it in a message
+
+def lambda_handler(event, context):         #the function starts here
+ 
+    now = datetime.now()                    #assigned the variable 'now' to the datetime.now() method that gives the current time
+    time = now.strftime("%H:%M:%S ")        #will give us back the current time in hours, minutes, and seconds
+
+    sqs = boto3.client('sqs')               #code needed to call on the resource 'SQS'
+    
+    sqs.send_message(QueueUrl="https://sqs.us-east-1.amazonaws.com/785610249674/Week_15",MessageBody=time)    #will send the message to the URL
+    
+    return {
+        'statusCode': 200,                  #will return a status code of 200, letting us know that the code ran successfully
+        'body': json.dumps('MISSION COMPLETE!!!!!')
+    }
